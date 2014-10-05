090e5bb3c5a1b5e521d3007335b0eafdd856bd1d
Dieter Lunn <coder2000@gmail.com>
2014-10-05 01:30:29 -0500

    Setup basic bootstrapper and modern ui shell.

---
+++ AirManager/AirBootstrapper.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.Composition.Hosting;
+using System.Windows;
+using Microsoft.Practices.Prism.MefExtensions;
+
+namespace AirManager
+{
+    public class AirBootstrapper : MefBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.GetExportedValue<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+            Application.Current.MainWindow = (Window) Shell;
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureAggregateCatalog()
+        {
+            base.ConfigureAggregateCatalog();
+
+            AggregateCatalog.Catalogs.Add(new AssemblyCatalog(typeof (AirBootstrapper).Assembly));
+        }
+    }
+}
