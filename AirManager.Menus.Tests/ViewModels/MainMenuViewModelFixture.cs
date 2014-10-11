b025f92ebbe3a6bef9e84c248927751c4fcd24b3
Dieter Lunn <coder2000@gmail.com>
2014-10-11 14:45:59 -0500

    Added test project for menus

---
+++ AirManager.Menus.Tests/ViewModels/MainMenuViewModelFixture.cs
@@ -0,0 +1,19 @@
+﻿using AirManager.Menus.ViewModels;
+using Microsoft.Practices.Prism.Regions;
+using Moq;
+using Xunit;
+
+namespace AirManager.Menus.Tests.ViewModels
+{
+    public class MainMenuViewModelFixture
+    {
+        [Fact]
+        public void CanInitViewModel()
+        {
+            var regionManager = new Mock<IRegionManager>();
+            var viewModel = new MainMenuViewModel(regionManager.Object);
+
+            Assert.NotNull(viewModel);
+        }
+    }
+}
