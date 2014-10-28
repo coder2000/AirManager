f0980298597c375cd76f496542d9d67bf9d6bcf5
Dieter Lunn <coder2000@gmail.com>
2014-10-28 09:15:30 -0500

    Added infrastructure test.

---
+++ AirManager.Infrastructure.Tests/GameDataFixture.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using AirManager.Infrastructure.Models;
+using Xunit;
+
+namespace AirManager.Infrastructure.Tests
+{
+    public class GameDataFixture
+    {
+        [Fact, ExcludeFromCodeCoverage]
+        public void CanGetAndSetCountries()
+        {
+            var countries = new[] {new Country{Id = "CA", Name = "Canada"}, new Country {Id = "US", Name = "United States"}};
+
+            var data = new GameData();
+
+            data.Countries = countries;
+
+            Assert.Equal(data.Countries, countries);
+        }
+    }
+}
